53c13936d68a80d1afa4e3af9b328b3b99709461
Robin_ <77126392+TrinhRobin@users.noreply.github.com>
2024-12-14 12:22:02 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,3 +17,9 @@ def print_matrix(matrix):
     """
     for row in matrix:
         print(' '.join(map(str, row)))
+
+def read_grid_from_file(filename):
+    """Reads a grid from a text file and converts it into a list of lists."""
+    with open(filename, 'r') as file:
+        grid = [list(line.strip()) for line in file if line.strip()]
+    return grid
